e97ca882b8872556f6ca962be0f6c869db1258eb
MazeMarei <moazmazen9199@gmail.com>
2025-08-22 08:45:31 +0300

    implmenting storage stabel memories

--- src/social_canister/src/storage.rs
+++ src/social_canister/src/storage.rs
@@ -1,5 +1,5 @@
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
-use ic_stable_structures::storable::{self, Bound};
+use ic_stable_structures::storable::Bound;
 use ic_stable_structures::Storable;
 
 use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
@@ -12,7 +12,7 @@ use crate::types::{comments, group, group_members, likes, posts, tal3a, tal3a_me
 
 use candid::{Decode, Encode};
 
-type Memory = VirtualMemory<DefaultMemoryImpl>;
+type _Memory = VirtualMemory<DefaultMemoryImpl>;
 
 // * Implement the `Storable` trait for the structs.
 // * convert structs to bytes to be `Storable` stored in stable memory
@@ -152,48 +152,48 @@ thread_local! {
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
     // *  Primary stores
-    pub static GROUPS: RefCell<StableBTreeMap<String, group::Group, Memory>> =
+    pub static GROUPS: RefCell<StableBTreeMap<String, group::Group, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(0))),
         ));
 
-    pub static POSTS: RefCell<StableBTreeMap<String, posts::Post, Memory>> =
+    pub static POSTS: RefCell<StableBTreeMap<String, posts::Post, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(1))),
             )
         );
 
-    pub static COMMENTS: RefCell<StableBTreeMap<String, comments::Comments, Memory>> =
+    pub static COMMENTS: RefCell<StableBTreeMap<String, comments::Comments, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(2))),
             )
         );
 
-    pub static LIKES: RefCell<StableBTreeMap<String, likes::Likes, Memory>> = // String is a composite key with user ID and post ID
+    pub static LIKES: RefCell<StableBTreeMap<String, likes::Likes, _Memory>> = // String is a composite key with user ID and post ID
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(3))),
             )
     );
 
-    pub static TAL3A: RefCell<StableBTreeMap<String, tal3a::Tal3a, Memory>> =
+    pub static TAL3A: RefCell<StableBTreeMap<String, tal3a::Tal3a, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(4))),
             )
         );
-    
-    pub static GROUP_MEMBERS: RefCell<StableBTreeMap<String, group_members::GroupMembers, Memory>> =
+
+    pub static GROUP_MEMBERS: RefCell<StableBTreeMap<String, group_members::GroupMembers, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(5))),
             )
         );
 
-    pub static TAL3A_MEMBERS: RefCell<StableBTreeMap<String, tal3a_members::Tal3aMembers, Memory>> =
+    pub static TAL3A_MEMBERS: RefCell<StableBTreeMap<String, tal3a_members::Tal3aMembers, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(6))),
@@ -201,63 +201,63 @@ thread_local! {
         );
     // * Secondary stores for indexing
 
-    pub static POSTS_BY_GROUP: RefCell<StableBTreeMap<String, StringVec, Memory>> =
+    pub static POSTS_BY_GROUP: RefCell<StableBTreeMap<String, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(7))),
             )
         );
 
-    pub static POSTS_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, Memory>> =
+    pub static POSTS_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(8))),
             )
     );
 
-    pub static COMMENTS_BY_POST: RefCell<StableBTreeMap<String, StringVec, Memory>> =
+    pub static COMMENTS_BY_POST: RefCell<StableBTreeMap<String, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(9))),
             )
         );
 
-    pub static COMMENTS_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, Memory>> =
+    pub static COMMENTS_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(10))),
             )
         );
 
-    pub static LIKES_BY_POST: RefCell<StableBTreeMap<String, StringVec, Memory>> =
+    pub static LIKES_BY_POST: RefCell<StableBTreeMap<String, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(11))),
             )
         );
 
-    pub static LIKES_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, Memory>> =
+    pub static LIKES_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(12))),
             )
         );
 
-    pub static GROUPS_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, Memory>> =
+    pub static GROUPS_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(13))),
             )
         );
 
-    pub static TAL3A_BY_GROUP: RefCell<StableBTreeMap<String, StringVec, Memory>> =
+    pub static TAL3A_BY_GROUP: RefCell<StableBTreeMap<String, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(14))),
             )
         );
 
-    pub static TAL3A_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, Memory>> =
+    pub static TAL3A_BY_USER: RefCell<StableBTreeMap<Principal, StringVec, _Memory>> =
         RefCell::new(
             StableBTreeMap::init(
                 MEMORY_MANAGER.with(|m| m.borrow().get( MemoryId::new(15))),
