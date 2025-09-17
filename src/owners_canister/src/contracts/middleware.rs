090abd80a1ab634e291b9aecbfe7288fabcb80e5
MazeMarei <moazmazen9199@gmail.com>
2025-09-18 02:29:44 +0300

    adding middleware for the updateing methods

--- src/owners_canister/src/contracts/middleware.rs
+++ src/owners_canister/src/contracts/middleware.rs
@@ -4,6 +4,12 @@ use ic_cdk::inspect_message;
 
 #[inspect_message]
 fn inspect_message() {
+    ic_cdk::api::debug_print(format!(
+        "Inspecting message from: {} , method: {}",
+        ic_cdk::api::msg_caller(),
+        ic_cdk::api::msg_method_name()
+    ));
+
     let caller = ic_cdk::api::msg_caller();
     let method_name = ic_cdk::api::msg_method_name();
     // * Block anonymous users from calling any update and query methods
