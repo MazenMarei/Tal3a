fa031d876e137b48b4eb86a3ac5b5c8e29e64792
MazeMarei <moazmazen9199@gmail.com>
2025-09-18 01:33:26 +0300

    adding middleware feature

---
+++ src/owners_canister/src/contracts/middleware.rs
@@ -0,0 +1,18 @@
+use candid::Principal;
+use ic_cdk;
+use ic_cdk::inspect_message;
+
+#[inspect_message]
+fn inspect_message() {
+    let caller = ic_cdk::api::msg_caller();
+    let method_name = ic_cdk::api::msg_method_name();
+    // * Block anonymous users from calling any update and query methods
+    if caller == Principal::anonymous() {
+        ic_cdk::api::trap(&format!(
+            "Anonymous users cannot call update and query method: {}",
+            method_name
+        ));
+    }
+
+    ic_cdk::api::accept_message();
+}
--- src/owners_canister/src/contracts/mod.rs
+++ src/owners_canister/src/contracts/mod.rs
@@ -1,5 +1,7 @@
 pub mod owners;
 pub mod group_admins;
+pub mod middleware;
+
 
 pub use owners::*;
 pub use group_admins::*;
